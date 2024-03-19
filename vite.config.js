f6ffcebc00e5e631f34f799f0d2380ffb5adceb0
Jonas Larsson <jonas_larsson1@outlook.com>
2024-03-19 08:54:40 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,33 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+
   plugins: [react()],
+
+  server: {
+
+    proxy: {
+
+      // localhost:5173/api -> localhost:3000/
+
+      '/api': {
+
+        // url som vi vill skicka till, från vår react app
+
+        target: 'http://localhost:3000',
+
+        changeOrigin: true,
+
+        // tar bort /api/ från vår path
+
+        rewrite: (path) => path.replace(/^\/api/, '')
+
+      },
+
+    }
+
+  }
+
 })
