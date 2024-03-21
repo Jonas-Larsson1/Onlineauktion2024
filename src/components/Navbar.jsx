3d4078d939f3af2e64d1d669506c99a7787c7c2d
BenjaminSegel <benjamin_segelstrom@hotmail.com>
2024-03-21 09:32:16 +0100

    small push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,14 @@ import Button from "react-bootstrap/esm/Button";
 import "../pages/styles.css"
 
 export default function Navbar() {
-  const {loggedIn, setLoggedIn} = useContext(GlobalContext)
+  const {loggedIn, logout} = useContext(GlobalContext)
   return <>
     <nav>
       |&nbsp;
       <Link to={"/"}>Home</Link> |&nbsp;
       <Link to={"/secondPage"}>Andra sidan</Link> |&nbsp;
       {loggedIn ?  <button className="logout-btn" onClick={() => {
-            setLoggedIn(false)
+            logout()
       }}>Logout</button> :  <Link to={"/loginPage"}>Login</Link>}
       
     </nav>
