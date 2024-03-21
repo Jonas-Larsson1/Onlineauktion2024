840140b84aacd9e739983f0043f7f82824092b5a
polyalyameow <117765092+polyalyameow@users.noreply.github.com>
2024-03-21 13:02:39 +0100

    changed type of categories in db

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -13,16 +13,16 @@ const Categories = () => {
         setAuctionData(data);
     }, [data]);
     
-    const filteredCategories = auctionData ? auctionData.map((item) => (allCategories.includes(item.category) ? null : allCategories.push(item.category))) : [];
+    const filteredCategories = auctionData ? auctionData.map((item) => (item.category.map(i => allCategories.includes(i) ? null : allCategories.push(i)))) : [];
 
   return (
     <>
     <div className="container d-flex flex-row justify-content-between border border-secondary rounded p-2">
        <h3 className='px-3'>categories</h3>
        <Button  type="button" className="btn btn-primary btn-block" onClick={() => {setToggle(!toggle), filteredCategories}}><i className="bi bi-filter"></i></Button>
-       </div>
-       <div>
-       {toggle ? <div><ul>{allCategories.map((cat, index) => <li key={index}>{cat}</li>)}</ul></div> : null}
+    </div>
+    <div className='p-2'>
+       {toggle ? <div><div className='list-group'>{allCategories.map((cat, index) => <a key={index} className='list-group-item list-group-item-action' href="#" onClick={() => console.log("clicked")}>{cat}</a>)}</div></div> : null}
     </div>
         
     </>
