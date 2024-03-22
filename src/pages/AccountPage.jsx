21a6c7f3cd9d256bba2d6ea39e596838c8bfc577
starkeOnline <starke.robin@outlook.com>
2024-03-22 14:30:25 +0100

    Refactored the code into components instead

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Card from "../components/Card";
 
 export default function AccountPage() {
     const id = 1; // Tilfälligt id för att hämta in användare, byta ut senare till actually inloggad
@@ -19,23 +20,16 @@ export default function AccountPage() {
             <div className="pt-5" style={{ backgroundColor: "#41B3A3", minHeight: '100vh' }}>
 
                 {user ? (
-                    <div className="d-flex justify-content-center mx-auto p-2 border rounded fst-italic fw-bold w-25 p-3" style={{ background: "#C38D9E" }}>
-                        <h4 className="fst-italic fw-bold">Welcome back, {user.username}.</h4>
-                    </div>
+                    <Card><h4 className="fst-italic fw-bold">Welcome back, {user.username}.</h4></Card>
                 ) : (
                     <p>404</p>
                 )}
 
-                <div className="pt-5 d-flex justify-content-center" style={{ backgroundColor: "#41B3A3" }}>
-                    <div className="d-flex justify-content-center mx-auto p-2 border rounded fst-italic fw-bold w-25 p-3" style={{ background: "#C38D9E" }}>
-                        <h5 className="fst-italic fw-bold">Your ongoing bids.</h5>
-                    </div>
-                    <div className="d-flex justify-content-center mx-auto p-2 border rounded fst-italic fw-bold w-25 p-3" style={{ background: "#C38D9E" }}>
-                        <h5 className="fst-italic fw-bold">Your ongoing auctions.</h5>
-                    </div>
-                    <div className="d-flex justify-content-center mx-auto p-2 border rounded fst-italic fw-bold w-25 p-3" style={{ background: "#C38D9E" }}>
-                        <h5 className="fst-italic fw-bold">Your previous auctions.</h5>
-                    </div>
+
+                <div className="d-flex justify-content-center mt-5">
+                    <Card><h5 className="fst-italic fw-bold">Your ongoing bids.</h5></Card>
+                    <Card><h5 className="fst-italic fw-bold">Your ongoing auctions.</h5></Card>
+                    <Card><h5 className="fst-italic fw-bold">Your previous auctions.</h5></Card>
                 </div>
             </div>
         </>
