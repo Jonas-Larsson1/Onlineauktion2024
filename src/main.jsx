ab0b797bea39c62f354f7c60754741d1a78bd481
Jonas Larsson <jonas_larsson1@outlook.com>
2024-03-25 11:52:07 +0100

    super tiny bugfix in main I know don't hurt me please

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import 'bootstrap/dist/css/bootstrap.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
