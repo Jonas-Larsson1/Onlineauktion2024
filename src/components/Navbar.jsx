2e06d7000e54ca04bdd6ff5f7816806e794c88c6
djaevil <djaevil1@gmail.com>
2024-03-25 10:50:38 +0100

    merge main fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,19 +45,3 @@ const Navbar = () => {
 }
 
 export default Navbar;
-
-
-
-<<<<<<< HEAD
-export default function Navbar() {
-  return <>
-    <nav>
-      |&nbsp;
-      <Link to={"/"}>Home</Link> |&nbsp;
-      <Link to={"/secondPage"}>Andra sidan</Link> |&nbsp;
-      <Link to={"/auctions"}>Auctions</Link> |&nbsp;
-    </nav>
-  </>
-}
-=======
->>>>>>> main
--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -2,23 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import HomePage from "/src/HomePage.jsx";
 import Navbar from "./Navbar.jsx"
-<<<<<<< HEAD
 import ListPage from "../pages/ListPage.jsx"
-=======
 import Footer from "./Footer.jsx"
->>>>>>> main
 
 export default function Router() {
   return (
     <BrowserRouter>    <div className="App">
       <Navbar />
-<<<<<<< HEAD
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/secondPage" element={<SecondPage />} />
-        <Route path="/auctions" element={<ListPage />} />
-      </Routes>
-=======
       <div className="content">
         <Routes >
           <Route path="/" element={<HomePage />} />
@@ -26,7 +16,6 @@ export default function Router() {
       </div>
     </div>
     <Footer />
->>>>>>> main
     </BrowserRouter>
 
   );
