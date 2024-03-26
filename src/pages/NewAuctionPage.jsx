cdf2fbc087d33e93e703aa8e40915930c92244b2
polyalyameow <117765092+polyalyameow@users.noreply.github.com>
2024-03-26 13:34:20 +0100

    start date - end date fixed

--- src/pages/NewAuctionPage.jsx
+++ src/pages/NewAuctionPage.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -7,58 +7,63 @@ const NewAuctionPage = () => {
   const [endDate, setEndDate] = useState(null);
 
   const handleStartDateChange = date => {
-      setStartDate(date);
+    setStartDate(date);
   };
 
   const handleEndDateChange = date => {
-      setEndDate(date);
+    setEndDate(date);
   };
 
   return (
     <form className='w-100 d-flex justify-content-center align-items-center m-3'>
       <div className='w-50'>
         <div className='d-flex flex-column'>
-          <input         
-            type="text" 
+          <input
+            type="text"
             // value={searchValue} 
             // onChange={handleChange} 
-            className="form-control mb-2" 
-            placeholder="Title" 
+            className="form-control mb-2"
+            placeholder="Title"
             aria-label="Title" />
-            <input         
-            type="text" 
+          <input
+            type="text"
             // value={searchValue} 
             // onChange={handleChange} 
-            className="form-control mb-2" 
-            placeholder="Description" 
+            className="form-control mb-2"
+            placeholder="Description"
             aria-label="Description" />
         </div>
-        <div className="col">
-                    <label>Start Date:</label>
-                    <DatePicker
-                        selected={startDate}
-                        onChange={handleStartDateChange}
-                        selectsStart
-                        startDate={startDate}
-                        endDate={endDate}
-                        minDate={new Date()}
-                        maxDate={endDate}
-                        className="form-control custom-date-picker "
-                    />
-                </div>
-                <div className="col">
-                    <label>End Date:</label>
-                    <DatePicker
-                        selected={endDate}
-                        onChange={handleEndDateChange}
-                        selectsEnd
-                        startDate={startDate}
-                        endDate={endDate}
-                        minDate={startDate}
-                        className="form-controlm custom-date-picker "
-                    />
-                </div>
-
+        <div className='row'>
+          <div className="col">
+          <div className='d-flex flex-column'>
+            <label>Start Date:</label>
+            <DatePicker
+              selected={startDate}
+              onChange={handleStartDateChange}
+              selectsStart
+              // startDate={startDate}
+              // endDate={endDate}
+              minDate={new Date()}
+              maxDate={endDate}
+              className="form-control custom-date-picker "
+            />
+            </div>
+          </div>
+          <div className="col">
+          <div className='d-flex flex-column'>
+            <label>End Date:</label>
+            <DatePicker
+              selected={endDate}
+              onChange={handleEndDateChange}
+              selectsEnd
+              startDate={startDate}
+              // endDate={endDate}
+              minDate={new Date()} 
+              className="form-control custom-date-picker "
+            />
+            </div>
+          </div>
+        </div>
       </div>
     </form>
   )
