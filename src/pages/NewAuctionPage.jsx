75ce2f5db7db4dca960d71060324db5f8fe4f2eb
Jonas Larsson <jonas_larsson1@outlook.com>
2024-04-02 14:21:17 +0200

    formatting 2

--- src/pages/NewAuctionPage.jsx
+++ src/pages/NewAuctionPage.jsx
@@ -36,46 +36,52 @@ const NewAuctionPage = () => {
   async function postNewAuction(e){
     e.preventDefault()
     
-    if(imageInput[0].length >= 1 && mainTitle.length > 3 && description.length > 3 && startDate != null && 
-      endDate != null && startPrice.length > 0 && reservedPrice.length > 0 && title[0].length >= 1){
+    if(imageInput[0].length >= 1 && 
+      mainTitle.length > 3 && 
+      description.length > 3 && 
+      startDate != null && 
+      endDate != null && 
+      startPrice.length > 0 && 
+      reservedPrice.length > 0 && 
+      title[0].length >= 1) {
 
-        
         const res = await fetch("/api/auctions", {
           
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        sellerId : loggedIn,
-        images: [imageInput],
-        title: mainTitle,
-        description: description,
-        startDate: startDate,
-        endDate: endDate,
-        startingPrice: startPrice,
-        reservePrice: reservedPrice,
-        category: [title],
-        bidHistory : [{}]
-      })
-    })
-    if(res.ok){
-      console.log(res)
-      navigate("/")
-    }else{
-      console.log("något fick fel")
-    }
-  } else{
-    alert("too short")
-   
-  }
-    
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            sellerId : loggedIn,
+            images: [imageInput],
+            title: mainTitle,
+            description: description,
+            startDate: startDate,
+            endDate: endDate,
+            startingPrice: startPrice,
+            reservePrice: reservedPrice,
+            category: [title],
+            bidHistory : [{}]
+          })
+        })
+
+      if (res.ok) {
+        console.log(res)
+        navigate("/")
+      } else {
+        console.log("något fick fel")
+      }
+    } else {
+      alert("too short")
+    } 
   }
 
   const existingCategories = []
 
-
-
-  let filtered = data ? data.map((item) => item.category.map(i => existingCategories.includes(i) ? null : existingCategories.push(i)
-  )) : null
+  let filtered = data ? data.map((item) => 
+    item.category.map(i => 
+      existingCategories.includes(i) ? null 
+      : existingCategories.push(i)
+    )
+  ) : null
 
   const handleStartDateChange = date => {
     setStartDate(date);
@@ -97,6 +103,7 @@ const NewAuctionPage = () => {
     <form className='w-100 d-flex justify-content-center align-items-center m-3'>
       <div className='d-flex flex-column' style={{width:"30%"}}>
         <div className='d-flex flex-column'>
+
           <input             
             type="text"
             value={imageInput} 
@@ -104,6 +111,7 @@ const NewAuctionPage = () => {
             className="form-control mb-2"
             placeholder="Link to your image"
             aria-label="Link to your image" />
+
           <input
             type="text"
             value={mainTitle} 
@@ -111,6 +119,7 @@ const NewAuctionPage = () => {
             className="form-control mb-2"
             placeholder="Title"
             aria-label="Title" />
+
           <input
             type="text"
             value={description} 
@@ -118,10 +127,12 @@ const NewAuctionPage = () => {
             className="form-control mb-2"
             placeholder="Description"
             aria-label="Description" />
+
         </div>
         <div className='row'>
           <div className="col">
             <div className='d-flex flex-column'>
+
               <label>Start Date:</label>
               <DatePicker
                 selected={startDate}
@@ -133,10 +144,12 @@ const NewAuctionPage = () => {
                 maxDate={endDate}
                 className="form-control custom-date-picker "
               />
+
             </div>
           </div>
           <div className="col">
             <div className='d-flex flex-column'>
+
               <label>End Date:</label>
               <DatePicker
                 selected={endDate}
@@ -148,6 +161,7 @@ const NewAuctionPage = () => {
                 disabled={!startDate}
                 className="form-control custom-date-picker "
               />
+
             </div>
           </div>
         </div>
@@ -155,6 +169,7 @@ const NewAuctionPage = () => {
           <div className="col">
             <div className='input-group mt-2'>
               <span className="input-group-text" >€</span>
+
               <input
                 type="text"
                 value={startPrice} 
@@ -162,11 +177,13 @@ const NewAuctionPage = () => {
                 className="form-control"
                 placeholder="Start Price"
                 aria-label="Start Price" />
+
             </div>
           </div>
           <div className="col">
             <div className='input-group mt-2'>
               <span className="input-group-text" >€</span>
+
               <input
                 type="text"
                 value={reservedPrice} 
@@ -174,22 +191,42 @@ const NewAuctionPage = () => {
                 className="form-control"
                 placeholder="Reserved Price"
                 aria-label="Reserved Price" />
+
             </div>
           </div>
         </div>
-        {/* <div className="container d-flex flex-row justify-content-between border border-secondary rounded p-2 "> */}
         <div className="dropdown mt-2 w-100 d-flex justify-content-center">
-          {/* {category ? <h3 className='px-3'>{category}</h3> : <h3 className='px-3'>Select category</h3>} */}
-          <button  className="btn btn-secondary dropdown-toggle w-75" type="button" id="dropdownMenuButton1" 
-            data-bs-toggle="dropdown" aria-expanded="false"
-           onClick={() => { setDropdownOpen(prev => !prev), filtered }}>{title == '' ? 'Categories' : title}</button>
+          <button 
+            className="btn btn-secondary dropdown-toggle w-75" 
+            type="button" 
+            id="dropdownMenuButton1" 
+            data-bs-toggle="dropdown" 
+            aria-expanded="false"
+            onClick={() => { setDropdownOpen(prev => !prev), filtered }}>
+
+              {title == '' ? 'Categories' : title}
+
+          </button>
         </div>
         {dropdownOpen ? <div className='list-group w-75 align-self-center'>
           {existingCategories.map((cat, index) => 
-        <a key={index} className='list-group-item list-group-item-action text-center' href="#" onClick={() => {setDropdownOpen(false), setTitle(cat)}}>{cat}</a>
-        )}<input type='text' placeholder='Add custom category' value={customCategory}
-        onChange={(e) => setCustomCategory(e.target.value)} onKeyDown={handleKeyPress}/></div> : null}
-        <button className="btn btn-primary mt-3 w-75 align-self-center" onClick={postNewAuction}>Submit</button>
+            <a key={index} className='list-group-item list-group-item-action text-center' href="#" onClick={() => {
+              setDropdownOpen(false), setTitle(cat)}}>{cat}</a>
+          )}
+          
+          <input 
+            type='text' 
+            placeholder='Add custom category' 
+            value={customCategory}
+            onChange={(e) => setCustomCategory(e.target.value)} 
+            onKeyDown={handleKeyPress}/>
+
+          </div> 
+        : null}
+
+        <button className="btn btn-primary mt-3 w-75 align-self-center" onClick={postNewAuction}>
+          Submit
+        </button>
       </div>
     </form>
   )
