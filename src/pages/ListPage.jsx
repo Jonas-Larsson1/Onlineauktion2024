ce78ee9f0fa388a478bb51e9a7251f590c176353
djaevil <djaevil1@gmail.com>
2024-04-06 22:17:27 +0200

    Filtering W.I.P

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -8,12 +8,12 @@ import StyleCard from "../components/StyleCard.jsx";
 // Define the ListPage functional component.
 export default function ListPage() {
 
-  // Define a state variable 'list' to store the list of items, starting as an empty array.
   const [list, setList] = useState([]);
   const {show, setShow} = useContext(GlobalContext)
   const {hideAlert} = useContext(GlobalContext)
   const [user, setUser] = useState(null)
   const {loggedIn} = useContext(GlobalContext)
+  const [activeList, setActiveList] = useState([]);
 
   useEffect(() => {
     const getUserData = async () => {
@@ -23,7 +23,7 @@ export default function ListPage() {
     };
 
     getUserData();
-  }, []);
+  }, []); // Empty array = this effect runs only once after the initial render.
 
   // useEffect hook to fetch data from the server when the component mounts.
   useEffect(() => {
@@ -34,53 +34,95 @@ export default function ListPage() {
 
         // Update the 'list' state variable with the fetched data.
         setList(result);
+        setActiveList(list)
 
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
     
-    // Call the getData function to fetch data.
     getData();
-  }, []); // Empty dependency array means this effect runs only once after the initial render.
+  }, []); 
 
   // Sort the list by the length of the bidHistory array of each item if bidHistory exists.
   if (list.length > 0 && list[0].hasOwnProperty("bidHistory")) {
     list.sort((a, b) => b.bidHistory.length - a.bidHistory.length);
   }
-  
-  return (<>
-    {user ? 
-      <Alert show={show} variant="success" className="alert">
-        <Alert.Heading>Welcome to the exclusive online auction site <em>Peta</em>, {user.username} !</Alert.Heading>
-        <p>
-          Happy bidding!
-        </p>
-        <hr />
-        <Button onClick={() => hideAlert()} variant="outline-success">
+
+    // const newestFirst = list.toSorted((a, b) => {
+    //   a.endDate - b.endDate;
+    //   return
+    // });
+
+    const ongoing = list.filter((item) => {
+      return Date.now() < item.endDate;
+    });
+
+    const finished = list.filter((item) => {
+      return Date.now() > item.endDate;
+    });
+
+    
+  return (
+    <>
+      {user ? (
+        <Alert show={show} variant="success" className="alert">
+          <Alert.Heading>
+            Welcome to the exclusive online auction site <em>Peta</em>,{" "}
+            {user.username} !
+          </Alert.Heading>
+          <p>Happy bidding!</p>
+          <hr />
+          <Button onClick={() => hideAlert()} variant="outline-success">
             Close me
           </Button>
-      </Alert>
-      : "" }
+        </Alert>
+      ) : (
+        ""
+      )}
 
-    <div className="container pb-4 border-bottom border-dark">
-      <div className="d-flex justify-content-center">
+      <div className="container pb-4 border-bottom border-dark">
+        <div className="d-flex justify-content-center">
           <div className="col-md-4 p-2" key="0">
-          <div>
-            <h2 className="fs-1 ms-2 pb-3 text text-danger">Beast of the week!</h2>
-            {list.length > 0 && list[0].hasOwnProperty("bidHistory") && <ListCard item={list[0]} />}
+            <div>
+              <h2 className="fs-1 ms-2 pb-3 text text-danger">
+                Beast of the week!
+              </h2>
+              {list.length > 0 && list[0].hasOwnProperty("bidHistory") && (
+                <ListCard item={list[0]} />
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className="container">
-      <div className="row row-cols-1 row-cols-md-3 g-4">
-        {list.slice(1).map((item, index) => (
+      <div className="d-flex justify-content-center mt-4">
+        <div className="btn-group-lg">
+          <button type="button" className="btn btn-dark m-2" onClick={() => {
+            setActiveList(list)
+          }}>
+              All
+          </button>
+          <button type="button" className="btn btn-dark m-2" onClick={() => {
+            setActiveList(ongoing)
+          }}>
+            Ongoing
+          </button>
+          <button type="button" className="btn btn-dark m-2" onClick={() => {
+            setActiveList(finished)
+          }}>
+            Finished
+          </button>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+          {activeList.map((item, index) => (
             <StyleCard>
               <ListCard item={item} />
             </StyleCard>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
-  </>);
+    </>
+  );
 }
