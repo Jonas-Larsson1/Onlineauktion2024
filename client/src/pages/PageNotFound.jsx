3edebb99145148ceca7ea18bfb992721bbe2cf0b
Hoube14 <Hoube14@gmail.com>
2024-04-18 11:33:15 +0200

    changed img

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -6,7 +6,7 @@ export default function PageNotFound() {
       <h1 className="d-flex justify-content-center">Page Not Found</h1>
       <div className="d-flex justify-content-center">
         <img
-          src="https://shirtoid.com/wp-content/uploads/2021/06/These-Are-Not-the-Droids.jpg"
+          src="https://wasmormon.org/wp-content/uploads/2023/04/image-4.jpg"
           alt=""
         />
       </div>
