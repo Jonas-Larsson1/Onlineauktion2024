fc934dc6e581d98c72f476b3c4f54cf7547209b1
Hoube14 <Hoube14@gmail.com>
2024-04-18 13:25:45 +0200

    added function

--- client/src/pages/AuctionPage.jsx
+++ client/src/pages/AuctionPage.jsx
@@ -5,46 +5,50 @@ import { GlobalContext } from "../GlobalContext";
 import ImageGallery from "../components/ImageGallery";
 import Bidding from "../components/Bidding";
 import AddToWatchList from "../components/AddToWatchList";
-
-
+import PageNotFound from "./PageNotFound";
 
 export default function AuctionPage() {
   let { id } = useParams();
   const [auction, setAuction] = useState(null);
 
   const updateAuction = (updatedAuction) => {
-    setAuction({...updatedAuction})
-  }
+    setAuction({ ...updatedAuction });
+  };
 
   useEffect(() => {
     const getData = async () => {
       const response = await fetch(`/api/auction/${id}`);
       const result = await response.json();
-      
 
-      if (result.bidHistory.length === 0 || Object.keys(result.bidHistory[0]).length === 0) {
-        result.bidHistory = [{
-          time: result.startDate,
-          userId: "Auction start" , 
-          amount: Number(result.startingPrice)
-        }]
-      }else {
+      if (
+        result.bidHistory.length === 0 ||
+        Object.keys(result.bidHistory[0]).length === 0
+      ) {
+        result.bidHistory = [
+          {
+            time: result.startDate,
+            userId: "Auction start",
+            amount: Number(result.startingPrice),
+          },
+        ];
+      } else {
         let cloneBidHistory = result.bidHistory;
-        for(let i = 0; i <= cloneBidHistory.length - 1; i++) {
-          if(cloneBidHistory[i].userId == "Auction start") {
-            result.bidHistory[i]["username"] = "Auction start"
-          }else {
-            const res = await fetch(`/api/user/getUsername/${cloneBidHistory[i].userId}`)
-            const username = await res.json()
-            result.bidHistory[i]["username"] = username
-          }          
+        for (let i = 0; i <= cloneBidHistory.length - 1; i++) {
+          if (cloneBidHistory[i].userId == "Auction start") {
+            result.bidHistory[i]["username"] = "Auction start";
+          } else {
+            const res = await fetch(
+              `/api/user/getUsername/${cloneBidHistory[i].userId}`
+            );
+            const username = await res.json();
+            result.bidHistory[i]["username"] = username;
+          }
         }
       }
       setAuction(result);
     };
 
     getData();
-
   }, []);
 
   return (
@@ -59,16 +63,20 @@ export default function AuctionPage() {
                   <ImageGallery auction={auction} />
                   <Card.Title>{auction.title}</Card.Title>
                   <Card.Text>{auction.description}</Card.Text>
-                    <Card className="my-3" border="light" style={{ padding: "1rem" }}>
-                      Auction duration:{" "}
-                      <b>
-                        {formatDateTime(auction.startDate)}
-                        &nbsp; - &nbsp;
-                        {formatDateTime(auction.endDate)}
-                      </b>
-                      <br />
-                    </Card>
-                  <AddToWatchList auction={auction}/>
+                  <Card
+                    className="my-3"
+                    border="light"
+                    style={{ padding: "1rem" }}
+                  >
+                    Auction duration:{" "}
+                    <b>
+                      {formatDateTime(auction.startDate)}
+                      &nbsp; - &nbsp;
+                      {formatDateTime(auction.endDate)}
+                    </b>
+                    <br />
+                  </Card>
+                  <AddToWatchList auction={auction} />
                 </Card.Body>
               </Card>
             </Col>
@@ -80,22 +88,21 @@ export default function AuctionPage() {
           </Row>
         </div>
       ) : (
-        <p>404: Auction not found</p>
+        PageNotFound()
       )}
     </>
   );
-  
 }
 
 export function formatDateTime(unixTimestamp) {
-  const date = new Date(unixTimestamp * 1000); 
+  const date = new Date(unixTimestamp * 1000);
   const options = {
-    year: 'numeric',
-    month: 'short',
-    day: '2-digit',
-    hour: 'numeric',
-    minute: 'numeric'
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+    hour: "numeric",
+    minute: "numeric",
   };
 
-  return date.toLocaleString('en-GB', options);
-};
+  return date.toLocaleString("en-GB", options);
+}
