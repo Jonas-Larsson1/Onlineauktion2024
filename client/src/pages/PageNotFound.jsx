d89f4999c35f7b3c95ceefda625070acc8fccf3d
Hoube14 <Hoube14@gmail.com>
2024-04-18 09:47:50 +0200

    spiced up page not found

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -1,7 +1,15 @@
+import React from "react";
+
 export default function PageNotFound() {
   return (
     <>
-      <h1>Page Not Found</h1>
+      <h1 className="d-flex justify-content-center">Page Not Found</h1>
+      <div className="d-flex justify-content-center">
+        <img
+          src="https://shirtoid.com/wp-content/uploads/2021/06/These-Are-Not-the-Droids.jpg"
+          alt=""
+        />
+      </div>
     </>
   );
 }
