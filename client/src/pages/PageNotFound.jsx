546bb60a8f74dea1a62615da1fbf3109ec92b008
Hoube14 <Hoube14@gmail.com>
2024-04-18 12:19:24 +0200

    changed pic

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -6,7 +6,7 @@ export default function PageNotFound() {
       <h1 className="d-flex justify-content-center">Page Not Found</h1>
       <div className="d-flex justify-content-center">
         <img
-          src="https://wasmormon.org/wp-content/uploads/2023/04/image-4.jpg"
+          src="https://i.pinimg.com/564x/f5/26/ce/f526ce6735569d480ef5e3563ceed889.jpg"
           alt=""
         />
       </div>
