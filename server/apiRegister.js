ae7ffe65627fd784072e0a0e8511ca780047577d
djaevil <djaevil1@gmail.com>
2024-04-22 13:40:39 +0200

    WIP api etc

--- server/apiRegister.js
+++ server/apiRegister.js
@@ -1,13 +1,15 @@
 import login from "./api/login.js";
 import users from "./api/users.js";
 import auctions from "./api/auctions.js";
-import bids from "./api/bids.js";
+// import bids from "./api/bids.js";
+import bidHistory from "./api/bidHistory.js";
 
 export default function (server, db) {
 
   users(server, db)
   login(server, db)
   auctions(server, db)
-  bids(server, db)
+  // bids(server, db)
+  bidHistory(server, db)
   
 }
--- server/model/BidHistory.js
+++ server/model/BidHistory.js
@@ -1,11 +1,19 @@
 import mongoose from "mongoose";
-import Bid from "./Bid";
+
+const bidSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+  time: Date,
+  amount: Number,
+  username: String
+});
+
+const Bid = mongoose.model("bid", bidSchema);
 
 const bidHistorySchema = new mongoose.Schema({
-    auctionId: {type: mongoose.Schema.Types.ObjectId, ref:"auctions"},
-    bids: [{type: [Bid.schema]}]
-    });
+  auctionId: { type: mongoose.Schema.Types.ObjectId, ref: "auctions" },
+  bids: [{ type: [Bid.schema] }]
+});
 
-const BidHistory = mongoose.model("bidHistory", bidHistorySchema); 
+const BidHistory = mongoose.model("bidHistory", bidHistorySchema);
 
 export default BidHistory;
