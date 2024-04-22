5b42b12df3a3847dea8f902aaca8698a4edfbf3d
djaevil <djaevil1@gmail.com>
2024-04-22 14:38:08 +0200

    WIP api etc 2

--- server/api/bidHistory.js
+++ server/api/bidHistory.js
@@ -10,4 +10,22 @@ export default function (server, db) {
         res.json(BidHistory.findById(req.params.id));
     });
 
+    server.put("/api/bidHistory/:id", async (req, res) => {
+      try {
+        const updatedBidHistory = await BidHistory.findByIdAndUpdate(
+          req.params.id,
+          req.body,
+          { new: false }
+        )
+
+        if (!updatedBidHistory) {
+          res.status(404).send("BidHistory not found");
+        } else {
+          res.status(200).send("BidHistory updated successfully");
+        }
+      } catch (error) {
+        res.status(500).send({ message: "Error updating BidHistory", error: error })
+      }
+    });
+
 }
