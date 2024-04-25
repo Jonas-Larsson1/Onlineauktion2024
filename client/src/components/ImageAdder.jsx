8f4625b20dabcbc9687d3e227728c303db0d4fce
Jonas Larsson <jonas_larsson1@outlook.com>
2024-04-25 14:39:23 +0200

    auction image editing moved to own component

---
+++ client/src/components/ImageAdder.jsx
@@ -0,0 +1,44 @@
+export default function ImageAdder ( {images, setAuctionData} ) {
+
+  const addImageInput = () => {
+    setAuctionData(prevState => ({
+      ...prevState,
+      allImages: [...prevState.allImages, ""]
+    }));
+  };
+
+  const removeImageInput = (index) => {
+    setAuctionData((prevState) => {
+      const updatedImages = prevState.allImages.filter((_, i) => i !== index);
+      return { ...prevState, allImages: updatedImages };
+    });
+  };
+  
+  return <>
+  {images.map((image, index) => (
+    <div key={index} className="d-flex align-items-center mb-1">
+      <input
+        key={index}
+        type="text"
+        value={image}
+        className="form-control mr-2"
+        onChange={(e) => onImageInput(index, e.target.value)}
+        placeholder="Link to your image"
+      />
+      {index > 0 ? 
+        <button
+        className="btn btn-danger"
+        onClick={(e) => {
+          e.preventDefault()
+          removeImageInput(index)
+        }}>-</button>
+      : <></>}
+    </div>
+  ))}
+
+    <button className="btn btn-primary mt-2 mb-4" onClick={(e) => {
+      e.preventDefault()
+      addImageInput()
+    }}>Click to add another image</button>
+  </>
+}
